9913544e167d58d87dc4858e7e353852d7fba390
liupeng <liupeng@dp.tech>
2024-04-09 20:41:30 +0800

    add demo message

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -3,6 +3,7 @@ import { ChatbotUIContext } from "@/context/context"
 import { Tables } from "@/supabase/types"
 import { FC, useContext, useState } from "react"
 import { Message } from "../messages/message"
+import { ChatMessage } from "@/types"
 
 interface ChatMessagesProps {}
 
@@ -13,7 +14,86 @@ export const ChatMessages: FC<ChatMessagesProps> = ({}) => {
 
   const [editingMessage, setEditingMessage] = useState<Tables<"messages">>()
 
-  return chatMessages
+  console.log("chatMessages", JSON.stringify(chatMessages, null, 2))
+
+  const MockMessageJson = [
+    {
+      message: {
+        id: "9f510699-5634-4751-b72b-3405f240b1c5",
+        chat_id: "a09ae80f-0334-4670-b507-cb68ceba1ddb",
+        user_id: "0ad9a610-e401-4d7f-a90e-87c48579887d",
+        created_at: "2024-04-09T09:27:49.021086+00:00",
+        updated_at: "2024-04-09T09:27:49.045953+00:00",
+        content: "Do sample docking",
+        image_paths: [],
+        model: "gpt-4-turbo-preview",
+        role: "user",
+        sequence_number: 0,
+        assistant_id: null
+      },
+      fileItems: []
+    },
+    {
+      message: {
+        id: "696e9042-937e-432f-ab2e-7fd26861ed9b",
+        chat_id: "a09ae80f-0334-4670-b507-cb68ceba1ddb",
+        user_id: "0ad9a610-e401-4d7f-a90e-87c48579887d",
+        created_at: "2024-04-09T09:27:49.021086+00:00",
+        updated_at: null,
+        content:
+          '### Config job uni-dock-21deefafdsafaf-dsf     \n<iframe src="https://toolbox.dp.tech/widgets/job/uni-dock-f763fd2c63ab438c9dc8cff0ba3831a2" width="600" height="400"></iframe>',
+        image_paths: [],
+        model: "gpt-4-turbo-preview",
+        role: "assistant",
+        sequence_number: 1,
+        assistant_id: null
+      },
+      fileItems: []
+    },
+    {
+      message: {
+        id: "cccd9372-6926-4cd3-b7db-31c382ccdeed",
+        chat_id: "a09ae80f-0334-4670-b507-cb68ceba1ddb",
+        user_id: "0ad9a610-e401-4d7f-a90e-87c48579887d",
+        created_at: "2024-04-09T09:30:09.314886+00:00",
+        updated_at: "2024-04-09T09:30:09.348087+00:00",
+        content: "提交",
+        image_paths: [],
+        model: "gpt-4-turbo-preview",
+        role: "user",
+        sequence_number: 2,
+        assistant_id: null
+      },
+      fileItems: []
+    },
+    {
+      message: {
+        id: "4fe6a047-bc19-4a4b-8b3a-a5d27ffa13ee",
+        chat_id: "a09ae80f-0334-4670-b507-cb68ceba1ddb",
+        user_id: "0ad9a610-e401-4d7f-a90e-87c48579887d",
+        created_at: "2024-04-09T09:30:09.314886+00:00",
+        updated_at: null,
+        content:
+          '### Confirm to submit uni-dock-21deefafdsafaf-dsf     \n<iframe src="https://toolbox.dp.tech/widgets/job/uni-dock-f763fd2c63ab438c9dc8cff0ba3831a2" width="600" height="400"></iframe>',
+        image_paths: [],
+        model: "gpt-4-turbo-preview",
+        role: "assistant",
+        sequence_number: 3,
+        assistant_id: null
+      },
+      fileItems: []
+    }
+  ]
+
+  // convert MockMessageJson to chatMessages of ChatMessage list
+  const mockMessages: ChatMessage[] = MockMessageJson.map(item => {
+    return {
+      message: item.message,
+      fileItems: item.fileItems
+    }
+  })
+
+  return mockMessages
     .sort((a, b) => a.message.sequence_number - b.message.sequence_number)
     .map((chatMessage, index, array) => {
       const messageFileItems = chatFileItems.filter(
--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
+const prod = process.env.NODE_ENV === 'production'
+
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true"
 })
 
 const withPWA = require("next-pwa")({
-  dest: "public"
+  dest: "public",
+  disable: prod ? false : true
 })
 
+// https://stackoverflow.com/questions/62224915/next-js-app-reloads-frequently-in-production
 module.exports = withBundleAnalyzer(
   withPWA({
     reactStrictMode: true,
